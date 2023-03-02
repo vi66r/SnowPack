b17c2bd93ad1513d53eb74bcd4fb2ae140df0244
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-02 15:34:36 -0500

    add exclusion for existing leftview to support a flag for country code in phone number textfield

--- Sources/SnowPack/Utilities/Extensions/UITextField+Extension.swift
+++ Sources/SnowPack/Utilities/Extensions/UITextField+Extension.swift
@@ -16,6 +16,12 @@ public extension UITextField {
         switch padding {
 
         case .left(let spacing):
+            if let leftView = leftView {
+                leftView.frame = CGRect(origin: leftView.frame.origin,
+                                        size: .init(width: leftView.frame.width + spacing,
+                                                    height: leftView.frame.height))
+                return
+            }
             let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: spacing, height: self.frame.height))
             self.leftView = leftPaddingView
             self.leftViewMode = .always
