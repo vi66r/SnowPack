23f4f8069da163f9a6b312f9dda1ddd42b342d47
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-02 15:30:25 -0500

    add padding/spacing rules to textfields

--- Sources/SnowPack/Utilities/Extensions/UITextField+Extension.swift
+++ Sources/SnowPack/Utilities/Extensions/UITextField+Extension.swift
@@ -1,6 +1,41 @@
 import UIKit
 
 public extension UITextField {
+    
+    enum PaddingSpace {
+        case left(CGFloat)
+        case right(CGFloat)
+        case equalSpacing(CGFloat)
+    }
+
+    func addPadding(padding: PaddingSpace) {
+
+        self.leftViewMode = .always
+        self.layer.masksToBounds = true
+
+        switch padding {
+
+        case .left(let spacing):
+            let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: spacing, height: self.frame.height))
+            self.leftView = leftPaddingView
+            self.leftViewMode = .always
+
+        case .right(let spacing):
+            let rightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: spacing, height: self.frame.height))
+            self.rightView = rightPaddingView
+            self.rightViewMode = .always
+
+        case .equalSpacing(let spacing):
+            let equalPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: spacing, height: self.frame.height))
+            // left
+            self.leftView = equalPaddingView
+            self.leftViewMode = .always
+            // right
+            self.rightView = equalPaddingView
+            self.rightViewMode = .always
+        }
+    }
+    
     func validate(as type: ValidatorType) throws -> String {
         let validator = Validators.validatorFor(type: type)
         return try validator.validate(text!)
