05b51064c6395f783765487713df8e23f0f50a18
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-03 15:09:19 -0500

    present on top

--- Sources/SnowPack/Utilities/Extensions/UIViewController+Extension.swift
+++ Sources/SnowPack/Utilities/Extensions/UIViewController+Extension.swift
@@ -1,6 +1,34 @@
 import UIKit
 
 extension UIViewController {
+    
+    func presentOnTop(_ destinationViewController: UIViewController, animated: Bool) {
+        if let presentedViewController = presentedViewController {
+            presentedViewController.presentOnTop(destinationViewController, animated: animated)
+        } else {
+            present(destinationViewController, animated: animated)
+        }
+    }
+    
+    func addDoneButtonOnKeyboard(title: String = "Done", to textField: UITextField, tapped: @escaping () -> Void = {}) {
+        let doneToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done = UIBarButtonItem(primaryAction: UIAction(title: "Done", handler: { _ in
+            textField.resignFirstResponder()
+            tapped()
+        }))
+        
+        done.style = .done
+        done.tintColor = .gray
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        textField.inputAccessoryView = doneToolbar
+    }
+    
     func presentAsMediumDetent(
         _ viewController: UIViewController,
         style: DetentedModalContainerController.Style = .light,
