711ec67d04a80d8a1731157e24af2376f3ebc9bd
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-09 23:17:54 -0800

    remove sessionary constraints from the viewmodel

--- Sources/SnowPack/Core/ViewModel.swift
+++ Sources/SnowPack/Core/ViewModel.swift
@@ -3,7 +3,6 @@ import UIKit
 
 /// Base class that implements common functionality of all ViewModels
 open class ViewModel {
-    @Dependency public var session: Session
     public let initialLoadEvent = PassthroughSubject<Void, Never>()
     public let refreshEvent = PassthroughSubject<Void, Never>()
     public let navigationEvent = PassthroughSubject<UIViewController, Never>()
