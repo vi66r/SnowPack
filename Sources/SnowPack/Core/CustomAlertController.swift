d8c6d85d4779f3f7d5debdc7d01fbdc36ca06d9a
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-13 00:41:41 -0700

    finetune animation timing

--- Sources/SnowPack/Core/CustomAlertController.swift
+++ Sources/SnowPack/Core/CustomAlertController.swift
@@ -93,7 +93,7 @@ public class CustomAlertController: UIViewController {
     func animateTransition() {
         horizontalOffset?.constant = 0
         verticalOffset?.constant = 0
-        UIView.animate(withDuration: 0.25,
+        UIView.animate(withDuration: 0.35,
                        delay: 0.0,
                        usingSpringWithDamping: Darwin.M_E / .pi,
                        initialSpringVelocity: .pi / ((Darwin.M_E)**2.0),
@@ -118,7 +118,7 @@ public class CustomAlertController: UIViewController {
             horizontalOffset?.constant = SnowPackUI.screenWidth ?? 1000.0
         }
         
-        UIView.animate(withDuration: 0.25,
+        UIView.animate(withDuration: 0.35,
                        delay: 0.0,
                        usingSpringWithDamping: Darwin.M_E / .pi,
                        initialSpringVelocity: .pi / ((Darwin.M_E)**2.0),
