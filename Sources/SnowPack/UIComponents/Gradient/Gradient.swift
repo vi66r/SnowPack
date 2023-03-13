948deb3128a020f27ef14bc01eb1b30110c372ff
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-12 21:30:48 -0700

    can't make things public for my life...

--- Sources/SnowPack/UIComponents/Gradient/Gradient.swift
+++ Sources/SnowPack/UIComponents/Gradient/Gradient.swift
@@ -1,16 +1,16 @@
 import UIKit
 
-final class Gradient: UIView {
+public final class Gradient: UIView {
     
     let gradient = CAGradientLayer()
     
-    init(colors: [UIColor], type: CAGradientLayerType = .axial) {
+    public init(colors: [UIColor], type: CAGradientLayerType = .axial) {
         super.init(frame: .zero)
         gradient.colors = colors.map({ $0.cgColor })
         gradient.type = type
     }
     
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
