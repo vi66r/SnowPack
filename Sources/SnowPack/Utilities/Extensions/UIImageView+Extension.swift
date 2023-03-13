d78d77586a9a367c6c234b3cd916dca309ad8de4
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-13 04:02:56 -0700

    make image loading better

--- Sources/SnowPack/Utilities/Extensions/UIImageView+Extension.swift
+++ Sources/SnowPack/Utilities/Extensions/UIImageView+Extension.swift
@@ -4,24 +4,36 @@ import UIKit
 
 public extension UIImageView {
     
-    static func loadingImage(from url: URL, placeholder: UIImage? = nil) -> UIImageView {
+    static func loadingImage(from url: URL,
+                             placeholder: UIImage? = nil,
+                             then: @escaping RemoteTypedAction<UIImage> = { _ in }) -> UIImageView {
         let imageView = UIImageView()
         imageView.image = placeholder
         var options = ImageLoadingOptions()
         options.pipeline = ImagePipeline.shared
         options.transition = .fadeIn(duration: 0.25)
         
-        NukeExtensions.loadImage(with: url, options: options, into: imageView)
+        NukeExtensions.loadImage(with: url, options: options, into: imageView) { result in
+            if case let .success(imageResponse) = result {
+                then(imageResponse.image)
+            }
+        }
         return imageView
     }
     
-    func loadImage(from url: URL, placeholder: UIImage? = nil) {
+    func loadImage(from url: URL,
+                   placeholder: UIImage? = nil,
+                   then: @escaping RemoteTypedAction<UIImage> = { _ in }) {
         self.image = placeholder
         var options = ImageLoadingOptions()
         options.pipeline = ImagePipeline.shared
         options.transition = .fadeIn(duration: 0.25)
         
-        NukeExtensions.loadImage(with: url, options: options, into: self)
+        NukeExtensions.loadImage(with: url, options: options, into: self) { result in
+            if case let .success(imageResponse) = result {
+                then(imageResponse.image)
+            }
+        }
     }
     
     func fade(to image: UIImage?) {
