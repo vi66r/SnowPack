8042b482070d4523bec29b6e33deb8203d9a2f20
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-14 03:08:08 -0700

    lul

--- Sources/SnowPack/Core/ViewController.swift
+++ Sources/SnowPack/Core/ViewController.swift
@@ -122,7 +122,7 @@ open class ViewController: UIViewController, Loading {
         case is SimpleWebViewController:
             let navigationController = NavigationController(rootViewController: viewController)
             navigationController.modalPresentationStyle = .overFullScreen
-            present(viewController, animated: true)
+            present(navigationController, animated: true)
         default:
             if let navigationController = navigationController {
                 navigationController.pushViewController(viewController, animated: true)
