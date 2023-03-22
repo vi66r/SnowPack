bdb22a3cca9c3d363ede9123100fcdcc3cb24ede
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-21 20:21:03 -0400

    LOL

--- Sources/SnowPack/Utilities/Extensions/String+Extension.swift
+++ Sources/SnowPack/Utilities/Extensions/String+Extension.swift
@@ -26,6 +26,46 @@ public extension String {
         return String(filter { !unacceptableCharacters.contains($0) })
     }
     
+    func levenshteinDistanceTo(_ str2: String) -> Int {
+        let str1 = self
+        let str1Length = str1.count
+        let str2Length = str2.count
+        
+        if str1Length == 0 {
+            return str2Length
+        }
+        
+        if str2Length == 0 {
+            return str1Length
+        }
+        
+        var distanceMatrix = Array(repeating: Array(repeating: 0, count: str2Length + 1), count: str1Length + 1)
+        
+        for i in 0...str1Length {
+            distanceMatrix[i][0] = i
+        }
+        
+        for j in 0...str2Length {
+            distanceMatrix[0][j] = j
+        }
+        
+        let str1Array = Array(str1)
+        let str2Array = Array(str2)
+        
+        for i in 1...str1Length {
+            for j in 1...str2Length {
+                let cost = str1Array[i - 1] == str2Array[j - 1] ? 0 : 1
+                distanceMatrix[i][j] = Swift.min(
+                    distanceMatrix[i - 1][j] + 1,
+                    distanceMatrix[i][j - 1] + 1,
+                    distanceMatrix[i - 1][j - 1] + cost
+                )
+            }
+        }
+        
+        return distanceMatrix[str1Length][str2Length]
+    }
+    
     static func friendlyBigNumber(from number: Double) -> String {
         let thousands = number/1000
         let millions = number/1000000
