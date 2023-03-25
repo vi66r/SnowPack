5b1e5b2823e363d7264f421055c505247f45a705
Rafi Rizwan <vi66raptor@gmail.com>
2023-03-25 17:13:42 -0400

    lel

--- Sources/SnowPack/UIComponents/TextView/TextView.swift
+++ Sources/SnowPack/UIComponents/TextView/TextView.swift
@@ -7,6 +7,18 @@ open class TextView: UITextView {
     var cancellables = Set<AnyCancellable>()
     public var pauseTime: Int = 1000
     
+    public func automaticallyScrollToCursor() {
+        let publisher = NotificationCenter.default.publisher(for: UITextView.textDidChangeNotification, object: self)
+        publisher
+            .sink(receiveValue: { (value) in
+                DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+                    guard let self = self else { return }
+                    let caret = self.caretRect(for: self.selectedTextRange!.start)
+                    self.scrollRectToVisible(caret, animated: true)
+                }
+            }).store(in: &cancellables)
+    }
+    
     public func observePauseInTyping() {
         let publisher = NotificationCenter.default.publisher(for: UITextView.textDidChangeNotification, object: self)
         publisher
