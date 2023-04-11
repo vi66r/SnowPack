3a23a9f2777044c7cb2602409b364a880c03e4d5
Rafi Rizwan <vi66raptor@gmail.com>
2023-04-11 18:15:29 -0400

    collectionview snapping layout

---
+++ Sources/SnowPack/UIComponents/SimpleCollectionView/SnappingCollectionViewLayout.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+// https://stackoverflow.com/a/43637969
+
+open class SnappingCollectionViewLayout: UICollectionViewFlowLayout {
+
+    open override func targetContentOffset(
+        forProposedContentOffset proposedContentOffset: CGPoint,
+        withScrollingVelocity velocity: CGPoint
+    ) -> CGPoint {
+        guard let collectionView = collectionView else {
+            return super.targetContentOffset(
+                forProposedContentOffset: proposedContentOffset,
+                withScrollingVelocity: velocity
+            )
+        }
+
+        var offsetAdjustment = CGFloat.greatestFiniteMagnitude
+        let horizontalOffset = proposedContentOffset.x + collectionView.contentInset.left
+
+        let targetRect = CGRect(
+            x: proposedContentOffset.x,
+            y: 0,
+            width: collectionView.bounds.size.width,
+            height: collectionView.bounds.size.height
+        )
+
+        let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
+
+        layoutAttributesArray?.forEach({ (layoutAttributes) in
+            let itemOffset = layoutAttributes.frame.origin.x
+            if fabsf(Float(itemOffset - horizontalOffset)) < fabsf(Float(offsetAdjustment)) {
+                offsetAdjustment = itemOffset - horizontalOffset
+            }
+        })
+
+        return CGPoint(x: proposedContentOffset.x + offsetAdjustment, y: proposedContentOffset.y)
+    }
+}
+
+public extension UICollectionViewLayout {
+    static var snappingLayout: UICollectionViewLayout {
+        SnappingCollectionViewLayout()
+    }
+}
