bb5bf4e25a7ae85724c486a4fec81239df0c9def
Rafi Rizwan <vi66raptor@gmail.com>
2023-04-12 07:16:07 -0400

    string ui calculation stuff

--- Sources/SnowPack/Utilities/Extensions/String+Extension.swift
+++ Sources/SnowPack/Utilities/Extensions/String+Extension.swift
@@ -86,4 +86,21 @@ public extension String {
       let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
       return String((0..<length).map{ _ in letters.randomElement()! })
     }
+    
+    func widthOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
+    }
+
+    func heightOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.height
+    }
+
+    func sizeOfString(usingFont font: UIFont) -> CGSize {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        return self.size(withAttributes: fontAttributes)
+    }
 }
